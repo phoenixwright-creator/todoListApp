636b3f7367d7a2f040f0ef91090137e76264d131
phoenixwright-creator <phoenixwright725@gmail.com>
2022-04-07 17:06:39 +0200

    Auto size cards, add a new project functionality, and styled the projects cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles.css';
 import Logo from './todoLogo.png';
 
+let id = 1;
+
 function addHeader(){
     const header = document.createElement('header');
     header.id = 'header';
@@ -45,24 +47,84 @@ function addProjectTabs(){
     return nav;
 }
 
+function addNewProjectButton(){
+    const newProjectDiv = document.createElement('div');
+    newProjectDiv.id = 'newProjectDiv';
+
+    const newProjectButton = document.createElement('button');
+    newProjectButton.id = 'newProjectButton';
+    newProjectButton.innerHTML = 'New Project';
+    newProjectDiv.appendChild(newProjectButton);
+    
+    return newProjectDiv;
+}
+
+function createNewProject(){
+    const allProjectsDiv = document.getElementById('allProjectsDiv');
+    allProjectsDiv.appendChild(createCard(id));
+    id++;
+    const allCards = document.querySelectorAll('.card');
+    console.log(allCards);
+}
+
+function createCard(id){
+    
+    const project = document.createElement('div');
+    project.id = 'project' + id;
+    project.className = 'card';
+    
+    const title = document.createElement('h3');
+    title.id = 'title' + id;
+    title.innerHTML = 'Project ' + id;
+    project.appendChild(title);
+
+    const summary = document.createElement('p');
+    summary.id = 'summary' + id;
+    summary.innerHTML = 'This is the description of the project ' + id;
+    project.appendChild(summary);
+
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.id = 'buttons' + id;
+    buttonsDiv.className = 'buttonsDiv';
+
+    const goToBtn = document.createElement('button');
+    goToBtn.id = 'goTo' + id;
+    goToBtn.className = 'cardButton goTo';
+    goToBtn.innerHTML = 'Go to project';
+    buttonsDiv.appendChild(goToBtn);
+
+    const modifyBtn = document.createElement('button');
+    modifyBtn.id = 'modify' + id;
+    modifyBtn.className = 'cardButton modify';
+    modifyBtn.innerHTML = 'Modify details';
+    buttonsDiv.appendChild(modifyBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.id = 'delete' + id;
+    deleteBtn.className = 'cardButton delete';
+    deleteBtn.innerHTML = 'Delete project';
+    buttonsDiv.appendChild(deleteBtn);
+
+    project.appendChild(buttonsDiv);
+
+    return project;
+}
+
 function displayAllProjects(){
     const allProjectsDiv = document.createElement('div');
     allProjectsDiv.id = 'allProjectsDiv';
 
-    const project1 = document.createElement('div');
-    project1.id = 'project1card';
-    project1.className = 'card';
-    allProjectsDiv.appendChild(project1);
+    allProjectsDiv.appendChild(createCard(id));
+    id++;
 
-    const project2 = document.createElement('div');
-    project2.id = 'project2card';
-    project2.className = 'card';
-    allProjectsDiv.appendChild(project2);
+    allProjectsDiv.appendChild(createCard(id));
+    id++;
 
     return allProjectsDiv;
 }
 
-
 document.getElementById('content').appendChild(addHeader());
 document.getElementById('content').appendChild(addProjectTabs());
+document.getElementById('content').appendChild(addNewProjectButton());
 document.getElementById('content').appendChild(displayAllProjects());
+document.getElementById('newProjectButton').addEventListener('click', createNewProject);
